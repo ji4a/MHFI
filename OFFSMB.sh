7d5040fe9f60e1d853e9f7a9e4077a06346d7aed
ji4a <117786715+ji4a@users.noreply.github.com>
2024-11-14 21:29:03 +0100

    Create OFFSMB.sh

---
+++ OFFSMB.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+
+# INSTALL SMB AND CREATE SHARE FOLDER
+
+dnf install -y samba samba-client
+
+mkdir /OFFSERVER
+chmod -R 777 /OFFSERVER
+
+echo "[SERVER]
+   path = /OFFSERVER
+   writable = yes
+   guest ok = no
+   create mask = 0777
+   directory mask = 0777
+   valid users = @MHBT_GROUP" >> /etc/samba/smb.conf
+
+systemctl restart smb
+
+systemctl enable smb
+systemctl start smb
+
+firewall-cmd --permanent --add-service=samba
+firewall-cmd --reload
+
+echo "File server setup completed. SERVER folder is shared via SMB."
+
+
+# ADD USER and give SMB ACCESS
+USERNAME="roy"
+PASSWORD="Money22"
+
+useradd $USERNAME
+
+echo -e "$PASSWORD\n$PASSWORD" | passwd $USERNAME
+
+(echo "$PASSWORD"; echo "$PASSWORD") | smbpasswd -a $USERNAME
+
+chown -R $USERNAME:$USERNAME /OFFSERVER
+chmod -R 755 /OFFSERVER
+
+echo "User $USERNAME created and set up for sharing the folder."
+
+
+# CREATE THE MHBT_GROUP AND SET PERMISSIONS FOR THE SHARED FOLDER
+groupadd MHBT_GROUP
+
+chown :MHBT_GROUP /OFFSERVER
+chmod g+rw /OFFSERVER
+
+# ADD USERS TO GROUP
+usermod -aG MHBT_GROUP admin
+usermod -aG MHBT_GROUP roy
+
+echo "GROUP created and users added to the group with read and write permissions on /OFFSERVER folder."
+
+sudo systemctl restart smb
+
+
+# DISBALE SEL LINUX
+echo "Disabling SELinux..."
+cp /etc/selinux/config /etc/selinux/config.bak
+
+# Change SELINUX setting to disabled
+sed -i 's/^SELINUX=.*/SELINUX=disabled/' /etc/selinux/config
+
+
+# REBOOT SYSTEM
+COUNTDOWN=3
+
+echo "Rebooting in $COUNTDOWN seconds..."
+for ((i = COUNTDOWN; i > 0; i--)); do
+    echo "$i..."
+    sleep 1
+done
+
+echo "Rebooting now!"
+sudo reboot
